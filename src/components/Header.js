b6f1af5b2607e35f852a3debd9aa4942e2e88206
mecuboi <matthewd.jusuf@gmail.com>
2022-12-15 16:58:54 +1100

    adding tailwind to react

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header bg-dark">
+      <h1 className="text-3xl font-bold underline">Home</h1>
+    </header>
+  );
+}
+
+export default Header;
