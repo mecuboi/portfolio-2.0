1bf39eeb733b14c14b8b814aeba8bfb59e4cf8ed
mecuboi <matthewd.jusuf@gmail.com>
2022-12-16 01:06:19 +1100

    adding portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import Project from './Project'
+import Project from '../Project'
 
 function Portfolio() {
-    return(
+    return (
         <div>
-        <div className="grid grid-cols-12">
-            <h2 className="header text-gray-500 col-span-12 lg:text-2xl md:text-xl text-lg font-extrabold p-5">Works I've done:</h2>
-            
-        </div>
-        <div>
-            
-        </div>
+            <div className="grid grid-cols-12">
+                <h2 className="header text-gray-500 col-span-12 text-lg font-extrabold p-5">Works I've done:</h2>
+
+            </div>
+            <div className="grid grid-cols-12">
+                
+                    <Project name='Something'/>
+                    <Project name='Something1'/>
+                    <Project name='Something2'/>
+                    <Project name='Something3'/>
+                    <Project name='Something4'/>
+                    <Project name='Something5'/>
+
+            </div>
+            <div>
+
+            </div>
         </div>
     )
 }
