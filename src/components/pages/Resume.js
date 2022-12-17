648f57bb3933b474819ab1ba810af2141d38ed5e
mecuboi <matthewd.jusuf@gmail.com>
2022-12-17 20:04:19 +1100

    adding proficiencies in resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -10,22 +10,32 @@ function Resume() {
     const path = "../../../public/resume.pdf"
     return (
         <div className='grid grid-cols-12'>
-            
+
             <div className="col-span-12 m-auto mt-3">
-                <AllPages pdf={resumeLink} className=""/>
-                
+                <AllPages pdf={resumeLink}  />
+
+            </div>
+            <div className="col-span-12 md:col-start-4 md:col-end-10 text-gray-700 mt-1 border-dotted border-2 border-yellow-500 rounded-md px-8 py-3 bg-white shadow-md">
+                <p className="font-bold text-yellow-500 text-xl " id="title">Proficiencies:</p>
+                <ul className="font-medium text-gray-600 text-lg mt-1 list-disc">
+                    <li>Javascript</li>
+                    <li>CSS</li>
+                    <li>Bootstrap</li>
+                    <li>Tailwind</li>
+                    <li>React</li>
+                </ul>
             </div>
             <div className='col-span-12 m-auto mt-4'>
-            <a href={resumeLink}
-                download className="">
-                <button
-                    type="button"
-                    className="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 "
-                >
-                    Download
-                </button>
+                <a href={resumeLink}
+                    download className="">
+                    <button
+                        type="button"
+                        className="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 "
+                    >
+                        Download
+                    </button>
 
-            </a>
+                </a>
             </div>
         </div>
 
